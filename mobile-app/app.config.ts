1e2ba74c4c15f349caf8f30e3be91a9e18f9eec6
Ravi Kirana <ravikirana32@gmail.com>
2026-09-01 14:32:32 +0530

    Bug fixes

--- mobile-app/app.config.ts
+++ mobile-app/app.config.ts
@@ -2,7 +2,7 @@ import type { ExpoConfig } from "expo/config";
 const config: ExpoConfig = {
   name: "Chit Management",
   slug: "chit-management",
-  version: "2.0.0",
+  version: "2.0.1",
   orientation: "portrait",
   scheme: "chitmanagement",
   userInterfaceStyle: "light",
